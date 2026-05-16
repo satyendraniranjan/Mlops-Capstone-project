27259c4420f873e784424979d1445df7d1a5b107
EC2 Default User <ec2-user@ip-172-31-70-78.ec2.internal>
2026-05-16 10:58:28 +0000

    running cicd

--- flask_app/app.py
+++ flask_app/app.py
@@ -69,25 +69,27 @@ def normalize_text(text):
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/satyendra.niranjan123/Mlops-Capstone-project.mlflow/')
-dagshub.init(repo_owner='satyendra.niranjan123', repo_name='Mlops-Capstone-project', mlflow=True)
+#mlflow.set_tracking_uri('https://dagshub.com/satyendra.niranjan123/Mlops-Capstone-project.mlflow/')
+#dagshub.init(repo_owner='satyendra.niranjan123', repo_name='Mlops-Capstone-project', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-#dagshub_token = os.getenv("CAPSTONE_TEST")
-#if not dagshub_token:
-#    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-#os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-#os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-#dagshub_url = "https://dagshub.com"
-#repo_owner = "vikashdas770"
-#repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "satyendra.niranjan123"
+repo_name = "Mlops-Capstone-project"
+
 # Set up MLflow tracking URI
-#mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
 # -------------------------------------------------------------------------------------
 
 
